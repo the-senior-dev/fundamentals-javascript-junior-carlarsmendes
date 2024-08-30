647418193899600ec8d628858ec55caa5d593ae3
CarlaMendes <carla.mendes@waeg.com>
2024-08-30 20:03:57 +0100

    Task 1 finished Tests passed

--- src/01_primitives/02_isNullObject.js
+++ src/01_primitives/02_isNullObject.js
@@ -5,7 +5,7 @@
  * @returns {boolean} - True if the variable is null and typeof returns 'object'.
  */
 function isNullObject(variable) {
-    // Your code here
+    return variable === null && typeof variable === 'object';
   }
   
   module.exports = isNullObject;
